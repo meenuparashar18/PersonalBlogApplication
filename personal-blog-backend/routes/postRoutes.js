47703749d271dc7f438a001010e98456de3801d9
Vadaanya Bansal <vadaanyabansal@gmail.com>
2026-03-22 11:10:48 +0530

    postRoutes.js updated

--- personal-blog-backend/routes/postRoutes.js
+++ personal-blog-backend/routes/postRoutes.js
@@ -4,7 +4,7 @@
 const express = require('express');
 
 // 2. Import the controller functions we created.
-// We use object destructuring to pull the functions out of the exports object.
+// We use object destructuring to pull the functions out of the exports object from our controller file.
 const {
   createPost,
   getAllPosts,
@@ -18,20 +18,20 @@ const {
 const router = express.Router();
 
 // 4. Define the routes.
-// We can chain route definitions for the same endpoint path. This is clean and efficient.
+// We can chain route definitions for the same endpoint path. This is a clean and efficient best practice.
 
-// Routes for the collection endpoint ('/api/posts')
-// - A GET request to '/' will trigger the getAllPosts controller.
-// - A POST request to '/' will trigger the createPost controller.
+// Routes for the collection endpoint (which will be mounted at '/api/posts' in server.js)
+// A GET request to this path will trigger the getAllPosts controller.
+// A POST request to this path will trigger the createPost controller.
 router.route('/').get(getAllPosts).post(createPost);
 
-// Routes for the specific document endpoint ('/api/posts/:id')
-// The ':id' is a URL parameter that Express will capture for us.
-// - A GET request to '/:id' will trigger getPostById.
-// - A PATCH request to '/:id' will trigger updatePost. (PATCH is for partial updates)
-// - A DELETE request to '/:id' will trigger deletePost.
+// Routes for the specific document endpoint (e.g., '/api/posts/:id')
+// The ':id' is a URL parameter that Express will capture and make available in `req.params.id`.
+// A GET request to '/:id' will trigger getPostById.
+// A PATCH request to '/:id' will trigger updatePost. (PATCH is the standard for partial updates).
+// A DELETE request to '/:id' will trigger deletePost.
 router.route('/:id').get(getPostById).patch(updatePost).delete(deletePost);
 
 // 5. Export the router.
-// This makes our configured router available to be used in our main `server.js` file.
+// This makes our fully configured router available to be used in our main `server.js` file.
 module.exports = router;
